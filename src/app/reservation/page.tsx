938dfa1d782bd26d340d1345a2d771537f9a4b94
csizmarikdaniel <43723695+csizmarikdaniel@users.noreply.github.com>
2024-11-12 18:06:23 +0100

    11.12 -3

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,6 +1,12 @@
+import { api } from "@/trpc/server";
 import ReservationList from "../_components/profile/reservation-list";
+import { redirect } from "next/navigation";
 
-const ReservationsPage = () => {
+const ReservationsPage = async () => {
+  const session = await api.auth.getSession();
+  if (!session) {
+    redirect("/login");
+  }
   return <ReservationList />;
 };
 
